4c1a38bc8160670f7a70de93ad23511f60ade5c9
David Toth <toth.david.munka@gmail.com>
2021-03-20 12:22:23 +0100

    added the request service

--- src/main/java/Controller/Launcher.java
+++ src/main/java/Controller/Launcher.java
@@ -4,17 +4,28 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class DealerApp extends Application {
+public class Launcher extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/View/DealerDashboard.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/AgentDashboard.fxml"));
+        Parent root = loader.load();
 
-        Scene scene = new Scene(root, 800, 300);
+        Scene scene = new Scene(root, 25, Screen.getPrimary().getBounds().getHeight() * 0.90);
 
-        primaryStage.setTitle("Dealer Backend");
+        scene.setFill(Color.TRANSPARENT);
+        scene.getStylesheets().add("/Style/main.css");
+        scene.getStylesheets().add("/Style/agent.css");
+        primaryStage.setX(Screen.getPrimary().getBounds().getWidth() - 25);
+        primaryStage.setY(Screen.getPrimary().getBounds().getHeight()/2.0f);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
+        primaryStage.setAlwaysOnTop(true);
         primaryStage.show();
     }
 }
