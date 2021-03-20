bc3cc74eddabfc6897846ca7ea83a7da367e6172
David Toth <toth.david.munka@gmail.com>
2021-03-20 13:32:51 +0100

    Implemented agent dashboard with exit and move functions

--- src/main/java/Controller/AgentDashboardController.java
+++ src/main/java/Controller/AgentDashboardController.java
@@ -1,2 +1,40 @@
-package Controller;public class AgentDashBoardController {
+package Controller;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class AgentDashboardController {
+
+    @FXML
+    private Button moveButton;
+    @FXML
+    private GridPane rootGrid;
+
+    private final Stage primaryStage;
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    public AgentDashboardController(Stage parent_){
+        primaryStage = parent_;
+    }
+    @FXML
+    public void initialize() {
+        moveButton.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        moveButton.setOnMouseDragged(event -> {
+            primaryStage.setY(event.getScreenY() - yOffset);
+        });
+    }
+
+    public void quitApp(ActionEvent actionEvent) {
+        Platform.exit();
+        System.exit(0);
+    }
 }
--- src/main/java/Controller/Launcher.java
+++ src/main/java/Controller/Launcher.java
@@ -9,19 +9,26 @@ import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Callback;
 
 public class Launcher extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/AgentDashboard.fxml"));
+        loader.setControllerFactory(new Callback<Class<?>, Object>() {
+            @Override
+            public Object call(Class<?> param) {
+                return new AgentDashboardController(primaryStage);
+            }
+        });
         Parent root = loader.load();
 
-        Scene scene = new Scene(root, 25, Screen.getPrimary().getBounds().getHeight() * 0.90);
+        Scene scene = new Scene(root, 50, Screen.getPrimary().getBounds().getHeight() * 0.90);
 
         scene.setFill(Color.TRANSPARENT);
         scene.getStylesheets().add("/Style/main.css");
         scene.getStylesheets().add("/Style/agent.css");
-        primaryStage.setX(Screen.getPrimary().getBounds().getWidth() - 25);
+        primaryStage.setX(Screen.getPrimary().getBounds().getWidth() - 50);
         primaryStage.setY(Screen.getPrimary().getBounds().getHeight()/2.0f);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setScene(scene);
