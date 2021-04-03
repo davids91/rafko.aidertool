5cfa7a147cc548706db8e148dc5b04a7d0f200a4
David Toth <toth.david.munka@gmail.com>
2021-04-04 00:04:25 +0200

    Implemented multi-user interaction

--- app-agent/src/main/java/org/rafko/aidertool/appagent/models/AidRequestMenuButton.java
+++ app-agent/src/main/java/org/rafko/aidertool/appagent/models/AidRequestMenuButton.java
@@ -18,19 +18,97 @@ public class AidRequestMenuButton extends SplitMenuButton {
         setText(stringBuilder.toString());
 
         /* Available actions *//* TODO: realize actions */
-        if(request.getRequesterUUID().equals(stats.getUserName())){
-            MenuItem acceptReject = new MenuItem("Accept");
-            acceptReject.setVisible(false);
-            MenuItem cancel = new MenuItem("Cancel");
-            cancel.setOnAction(event -> client_.cancelRequest(request.getRequestID()));
-            getItems().addAll(acceptReject,cancel);
-        }
-        MenuItem ignore = new MenuItem("Ignore");
+        MenuItem initiate = new MenuItem(); /* Accept? Finalize? Prioritize(de-snooze)? */
+        MenuItem finalize = new MenuItem(); /* Help request finished */
+        MenuItem cancel = new MenuItem(); /* Remove the request? Take back help? */
         MenuItem snooze = new MenuItem("Snooze");
-        MenuItem finalize = new MenuItem("Finalize");
-        getItems().addAll(ignore,snooze,finalize);
+        MenuItem ignore = new MenuItem("Ignore");
+        initiate.setOnAction(event -> client_.initiateRequest(request.getRequestID()));
+        cancel.setOnAction(event -> client_.cancelRequest(request.getRequestID()));
+        if(request.getRequesterUUID().equals(stats.getUserName())){
+            snooze.setOnAction(event -> client_.postponeRequest(request.getRequestID()));
+            ignore.setVisible(false);
+            switch (request.getState()){
+                case STATE_OPEN:{
+                    initiate.setVisible(false);
+                    finalize.setVisible(false);
+                    cancel.setText("Delete my request");
+                }break;
+                case STATE_POSTPONED:{
+                    finalize.setVisible(false);
+                    initiate.setText("Resume..");
+                    cancel.setText("Delete my request");
+                }break;
+                case STATE_ACTIVE:{
+                    finalize.setText("I received the needed help");
+                    initiate.setVisible(false);
+                    cancel.setText("I can't focus on this request right now");
+                }break;
+                case STATE_PENDING:{
+                    if(request.getFinalizedBy().equals(stats.getUserName())){
+                        finalize.setText("I received the needed help");
+                    }else finalize.setVisible(false);
+                    initiate.setText("I accept the help I've been provided");
+                    cancel.setText("I still need help..");
+                }break;
+                default: initiate.setVisible(false);
+            }
+        }else{
+            snooze.setVisible(false); /* TODO: Implement "help will eventually be available" state */
+            switch (request.getState()){
+                case STATE_OPEN:{
+                    initiate.setText("I'd like to help!");
+                    ignore.setOnAction(event -> setVisible(false));
+                }break;
+                case STATE_POSTPONED:{
+                    initiate.setVisible(false);
+                    ignore.setOnAction(event -> setVisible(false));
+                }break; /* TODO: implement postponed aid request response */
+                case STATE_ACTIVE:{
+                    if(request.getHelperUUID().equals(stats.getUserName())){
+                        initiate.setText("I finished helping " + request.getRequesterUUID());
+                        cancel.setText("I can no longer help " + request.getRequesterUUID() + ":(");
+                        ignore.setVisible(false);
+                    }else{
+                        initiate.setVisible(false);
+                        cancel.setVisible(false);
+                    }
+                }break;
+                case STATE_PENDING:{
+                    initiate.setVisible(false);
+                    if(request.getHelperUUID().equals(stats.getUserName())){
+                        if(request.getFinalizedBy().equals(stats.getUserName())){
+                            cancel.setText("I am not done helping " + request.getRequesterUUID());
+                            finalize.setText("I agree with " + request.getRequesterUUID() + ", help have been provided!");
+                        }else{
+                            cancel.setVisible(false);
+                            finalize.setVisible(false);
+                        }
+                    }else{
+                        cancel.setVisible(false);
+                        initiate.setVisible(false);
+                        finalize.setVisible(false);
+                    }
+                }break;
+                default: initiate.setVisible(false);
+            }
+        }
 
-        /* Userdata and styling */
+        if(request.getRequesterUUID().equals(stats.getUserName())){ /* If the owner of the request is the user */
+            switch (request.getState()){
+                case STATE_OPEN:{getItems().addAll(snooze,cancel);}break;
+                case STATE_POSTPONED:
+                case STATE_ACTIVE:{getItems().addAll(initiate,cancel);}break;
+                case STATE_PENDING:{getItems().addAll(cancel);}break;
+            }
+        }else{
+            if(request.getState() == RequestDealer.HelpState.STATE_OPEN){
+                getItems().add(initiate);
+            }
+            getItems().add(ignore);
+        }
+
+        /* User data and styling */
         switch (request.getState()){
             case STATE_OPEN:setStyle("-fx-mark-color: green;"); break;
             case STATE_POSTPONED:setStyle("-fx-mark-color: gainsboro;"); break;
@@ -39,7 +117,6 @@ public class AidRequestMenuButton extends SplitMenuButton {
             case STATE_FINISHED:setStyle("-fx-mark-color: lightgreen;"); break;
             default: setStyle("-fx-mark-color: red;");
         }
-
         setUserData(request);
         setContentDisplay(ContentDisplay.RIGHT);
         setPopupSide(Side.LEFT);
--- app-dealer/src/main/java/org/rafko/aidertool/appdealer/services/DealerServer.java
+++ app-dealer/src/main/java/org/rafko/aidertool/appdealer/services/DealerServer.java
@@ -6,11 +6,13 @@ import io.grpc.stub.StreamObserver;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import org.rafko.aidertool.RequestDealer;
 import org.rafko.aidertool.RequestHandlerGrpc;
 import org.rafko.aidertool.appdealer.models.DealerStats;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +28,23 @@ public class DealerServer {
     public DealerServer(DealerStats stats_){
         stats = stats_;
         requests = new SimpleListProperty<>(FXCollections.observableArrayList(new ArrayList<>()));
-
+        requests.addListener((ListChangeListener<? super RequestDealer.AidRequest>) change -> {
+            while(change.next()){
+                if(change.wasAdded()){
+                    ArrayList<RequestDealer.AidRequest> toRemove = new ArrayList<>();
+                    for(RequestDealer.AidRequest newRequest : change.getAddedSubList()){
+                        if(
+                            (RequestDealer.HelpState.STATE_FINISHED == newRequest.getState())
+                            ||(RequestDealer.HelpState.STATE_UNKNOWN == newRequest.getState())
+                            ||(RequestDealer.HelpState.STATE_CANCELLED == newRequest.getState())
+                        ){
+                            toRemove.add(newRequest);
+                        }
+                    }
+                    if(0 < toRemove.size()) requests.removeAll(toRemove);
+                }
+            }
+        });
     }
 
     public void start(int port) throws IOException {
@@ -128,33 +146,192 @@ public class DealerServer {
         @Override
         public void initiate(RequestDealer.AidToken request, io.grpc.stub.StreamObserver<RequestDealer.AidToken> responseObserver) {
             LOGGER.log(Level.FINE,"initiate call received!");
+            RequestDealer.AidRequest storedRequest = getRequestByID(request.getRequestID());
+            if( /* If the given request is found */
+                (null != storedRequest)
+                &&( /* And can be initiated */
+                    ( /* The requester can not initiate its own request when it is open.. */
+                        (RequestDealer.HelpState.STATE_OPEN == storedRequest.getState())
+                        &&(!storedRequest.getRequesterUUID().equals(request.getUserUUID()))
+                    )
+                    ||( /* If it's postponed, only the requester can undo it.. */
+                        (RequestDealer.HelpState.STATE_POSTPONED == storedRequest.getState())
+                        &&(storedRequest.getRequesterUUID().equals(request.getUserUUID()))
+                    )
+                )
+            ){
+                switch (storedRequest.getState()){
+                    case STATE_OPEN:{
+                        RequestDealer.AidRequest newRequest = (
+                            RequestDealer.AidRequest.newBuilder(storedRequest)
+                            .setState(RequestDealer.HelpState.STATE_ACTIVE)
+                            .setHelperUUID(request.getUserUUID())
+                            .build()
+                        );
+                        requests.remove(storedRequest);
+                        requests.add(newRequest);
+                        responseObserver.onNext(getAcceptedResponse());
+                    }break;
+                    case STATE_POSTPONED:{
+                        RequestDealer.AidRequest newRequest = (
+                            RequestDealer.AidRequest.newBuilder(storedRequest)
+                            .setState(RequestDealer.HelpState.STATE_OPEN)
+                            .build()
+                        );
+                        requests.remove(storedRequest);
+                        requests.add(newRequest);
+                        responseObserver.onNext(getAcceptedResponse());
+                    }break;
+                    default: responseObserver.onNext(getRejectedResponse());
+                }
+            }
+            responseObserver.onCompleted();
         }
 
         @Override
         public void cancel(RequestDealer.AidToken request, io.grpc.stub.StreamObserver<RequestDealer.AidToken> responseObserver) {
             LOGGER.log(Level.FINE,"cancel request call received from : " + request.getUserUUID() + "!");
-            RequestDealer.AidToken response;
-            if(removeRequestByID(request.getRequestID())){
-                response = RequestDealer.AidToken.newBuilder()
-                .setState(RequestDealer.RequestResponse.QUERY_OK)
-                .build();
-            }else{
-                response = RequestDealer.AidToken.newBuilder()
-                .setState(RequestDealer.RequestResponse.QUERY_REJECTED)
-                .build();
+            RequestDealer.AidRequest storedRequest = getRequestByID(request.getRequestID());
+            if( /* The request is found */
+                (null != storedRequest)
+                &&(
+                    ( /* Only the requester can remove its request */
+                        ( /* In both pending and open states, the requester can cancel its request */
+                            (RequestDealer.HelpState.STATE_OPEN == storedRequest.getState())
+                            ||(RequestDealer.HelpState.STATE_POSTPONED == storedRequest.getState())
+                        )&&(storedRequest.getRequesterUUID().equals(request.getUserUUID()))
+                    )||(
+                        ( /* In case the request is active or pending */
+                            (RequestDealer.HelpState.STATE_ACTIVE == storedRequest.getState())
+                            ||(RequestDealer.HelpState.STATE_PENDING == storedRequest.getState())
+                        )&&( /* And one of the participants are requesting action */
+                            (storedRequest.getRequesterUUID().equals(request.getUserUUID()))
+                            ||(storedRequest.getHelperUUID().equals(request.getUserUUID()))
+                        )
+                    )
+                )
+            ){
+                switch(storedRequest.getState()){
+                    case STATE_OPEN:
+                    case STATE_POSTPONED:{
+                        RequestDealer.AidRequest newRequest = (
+                            RequestDealer.AidRequest.newBuilder(storedRequest)
+                            .setState(RequestDealer.HelpState.STATE_CANCELLED)
+                            .setHelperUUID("")
+                            .build()
+                        );
+                        requests.remove(storedRequest);
+                        requests.add(newRequest);
+                        responseObserver.onNext(getAcceptedResponse());
+                    }break;
+                    case STATE_ACTIVE:{
+                        RequestDealer.AidRequest newRequest = (
+                            RequestDealer.AidRequest.newBuilder(storedRequest)
+                            .setState(RequestDealer.HelpState.STATE_OPEN)
+                            .setHelperUUID("")
+                            .build()
+                        );
+                        requests.remove(storedRequest);
+                        requests.add(newRequest);
+                        responseObserver.onNext(getAcceptedResponse());
+                    }break;
+                    case STATE_PENDING:{
+                        RequestDealer.AidRequest newRequest = (
+                            RequestDealer.AidRequest.newBuilder(storedRequest)
+                            .setState(RequestDealer.HelpState.STATE_ACTIVE)
+                            .build()
+                        );
+                        requests.remove(storedRequest);
+                        requests.add(newRequest);
+                        responseObserver.onNext(getAcceptedResponse());
+                    }break;
+                    default: responseObserver.onNext(getRejectedResponse());
+                }
             }
-            responseObserver.onNext(response);
             responseObserver.onCompleted();
         }
 
         @Override
         public void postpone(RequestDealer.AidToken request, StreamObserver<RequestDealer.AidToken> responseObserver) {
-            LOGGER.log(Level.FINE,"postpone call received!");
+            LOGGER.log(
+                Level.FINE,
+                "postpone request call received from : " + request.getUserUUID() + " to request + "+request.getRequestID()+"!"
+            );
+            RequestDealer.AidRequest storedRequest = getRequestByID(request.getRequestID());
+            if( /* If the given request is found */
+                (null != storedRequest)
+                &&(request.getUserUUID().equals(storedRequest.getRequesterUUID())) /* The user originated the request */
+                &&( /* And the request can be postponed */
+                    (RequestDealer.HelpState.STATE_OPEN == storedRequest.getState())
+                    ||(RequestDealer.HelpState.STATE_ACTIVE == storedRequest.getState())
+                )
+            ){
+                RequestDealer.AidRequest newRequest = RequestDealer.AidRequest.newBuilder(storedRequest)
+                .setState(RequestDealer.HelpState.STATE_POSTPONED)
+                .build();
+                requests.remove(storedRequest);
+                requests.add(newRequest);
+                responseObserver.onNext(getAcceptedResponse());
+            }else{
+                responseObserver.onNext(getRejectedResponse());
+            }
+            responseObserver.onCompleted();
         }
 
         @Override
         public void finalize(RequestDealer.AidToken request, io.grpc.stub.StreamObserver<RequestDealer.AidToken> responseObserver) {
             LOGGER.log(Level.FINE,"finalize call received!");
+            RequestDealer.AidRequest storedRequest = getRequestByID(request.getRequestID());
+            if( /* The request is found */
+                (null != storedRequest)
+                &&(
+                    ( /* If the state is active, both the requester and helper can finalize */
+                        (RequestDealer.HelpState.STATE_ACTIVE == storedRequest.getState())
+                        &&(storedRequest.getRequesterUUID().equals(request.getUserUUID()))
+                        &&(storedRequest.getHelperUUID().equals(request.getUserUUID()))
+                    )||( /* If the request is pending */
+                        (RequestDealer.HelpState.STATE_PENDING == storedRequest.getState())
+                        &&( /* Depending on who initiated the request */
+                            ( /* In case the requester finalized already, the helper can finish it */
+                                (storedRequest.getFinalizedBy().equals(storedRequest.getRequesterUUID()))
+                                &&(storedRequest.getHelperUUID().equals(request.getUserUUID()))
+                            )||( /* In case the helper finalized it, the requester can finish it */
+                                (storedRequest.getFinalizedBy().equals(storedRequest.getHelperUUID()))
+                                &&(storedRequest.getRequesterUUID().equals(request.getUserUUID()))
+                            )
+                        )
+                    )
+                )
+            ){
+                switch (storedRequest.getState()){
+                    case STATE_ACTIVE:{
+                        RequestDealer.AidRequest newRequest = (
+                            RequestDealer.AidRequest.newBuilder(storedRequest)
+                            .setState(RequestDealer.HelpState.STATE_PENDING)
+                            .setFinalizedBy(request.getUserUUID())
+                            .build()
+                        );
+                        requests.remove(storedRequest);
+                        requests.add(newRequest);
+                        responseObserver.onNext(getAcceptedResponse());
+                    }break;
+                    case STATE_PENDING:{
+                        if(!storedRequest.getFinalizedBy().equals(request.getUserUUID())){
+                            RequestDealer.AidRequest newRequest = (
+                                RequestDealer.AidRequest.newBuilder(storedRequest)
+                                .setState(RequestDealer.HelpState.STATE_FINISHED)
+                                .setFinalizedBy(request.getUserUUID())
+                                .build()
+                            );
+                            requests.remove(storedRequest);
+                            requests.add(newRequest);
+                            responseObserver.onNext(getAcceptedResponse());
+                        }else responseObserver.onNext(getRejectedResponse());
+                    }break;
+                    default: responseObserver.onNext(getRejectedResponse());
+                }
+            }
+            responseObserver.onCompleted();
         }
 
         @Override
@@ -167,6 +344,18 @@ public class DealerServer {
 
     public ListProperty<RequestDealer.AidRequest> getRequests(){
         return requests;
+    }
+
+    private RequestDealer.AidToken getRejectedResponse(){
+        return RequestDealer.AidToken.newBuilder()
+        .setState(RequestDealer.RequestResponse.QUERY_REJECTED)
+        .build();
+    }
+
+    private RequestDealer.AidToken getAcceptedResponse(){
+        return RequestDealer.AidToken.newBuilder()
+        .setState(RequestDealer.RequestResponse.QUERY_OK)
+        .build();
     }
 
     public RequestDealer.AidRequest getRequestByID(String id){
